1c2e6eb4c0cc19547daadb17404d2812a13486be
cluffa <alexcluff16@gmail.com>
2022-04-09 22:28:20 +0000

    update

--- Weight-Loss-Trends/app.R
+++ Weight-Loss-Trends/app.R
@@ -2,40 +2,63 @@
 library(shiny)
 library(ggplot2)
 library(googlesheets4)
+library(plotly)
+library(randomForest)
 
-# Define UI for application that draws a histogram
+gs4_auth("alexcluff16@gmail.com", cache = ".secrets")
+df <- read_sheet("151vhoZ-kZCnVfIQ7h9-Csq1rTMoIgsOsyj_vDRtDMn0")
+
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Weight Loss Trends"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    titlePanel("Weight Loss Trend"),
+    dateRangeInput('dateRange',
+        label = 'Date range input: yyyy-mm-dd',
+        start = as.POSIXct("2021-03-01"), end = Sys.Date() + 1
+    ),
+    mainPanel(
+        plotlyOutput("plot1"),
+        verbatimTextOutput("summary"),
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    get_date_range <- reactive({
+        c(
+            as.POSIXct(input$dateRange[1]),
+            as.POSIXct(input$dateRange[2])
+        )
+    })
+    
+    get_df <- reactive({
+        range <- get_date_range()
+        df[df$date >= range[1] & df$date < range[2],]
+    })
+    
+    get_model <- reactive({
+        lm(weight ~ date, get_df())
+    })
+    
+    output$plot1 <- renderPlotly({
+        coefs <- get_model()$coefficients
+        range <- get_date_range()
+        df <- get_df()
+        
+        graph <- ggplot() +
+            geom_abline(intercept = coefs[1], slope = coefs[2], color = "red", linetype = 2) +
+            geom_point(aes(date, weight), df) +
+            xlim(range)
+        
+        graph %>% ggplotly()
+    })
+    
+    output$summary <- renderPrint({
+        model <- get_model()
+        coef <- unname(model$coefficients[2])*86400
+        
+        cat("Daily Trend:",coef,"","Weekly Trend:",coef*7, sep = "\n")
+        
+        summary(model)
     })
 }
 
