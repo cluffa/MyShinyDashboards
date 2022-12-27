e2cd6ce9bd6dd74f7d4710f5932bfa74db6af740
cluffa <alexcluff16@gmail.com>
2022-12-26 22:47:04 -0500

    date defaults to last 90 days

--- Weight-Loss-Trends/app.R
+++ Weight-Loss-Trends/app.R
@@ -16,7 +16,8 @@ ui <- dashboardPage(
         dateRangeInput(
           "dateRange",
           label = "Date Range:",
-          start = as.POSIXct("2022-07-01"), end = Sys.Date()
+          start = Sys.Date() - 90,
+          end = Sys.Date()
         ),
         plotOutput("plot1"),
       ),
