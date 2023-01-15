f676cbc7127bb60bcf2a30e1a696132d5aaf936d
cluffa <alexcluff16@gmail.com>
2023-01-14 21:06:49 -0500

    add debug output

--- deploy.R
+++ deploy.R
@@ -7,35 +7,51 @@ myCluster <- makeCluster(3, type = "PSOCK")
 registerDoParallel(myCluster)
 
 getenv <- function(name){
-  var <- Sys.getenv(name, unset = NA)
-  if(is.na(var)){
-    stop(paste0("cannot find ",name, "!"),call. = FALSE)
-  }
-  gsub("\"", '',var)
+    var <- Sys.getenv(name, unset = NA)
+    if(is.na(var)){
+        stop(paste0("cannot find ",name, "!"),call. = FALSE)
+    }
+    gsub("\"", '',var)
 }
 
 dirs <- c(
-  "IWF-Data-Explorer/",
-  "Weight-Loss-Trends/",
-  "R6-Stats/"
+    "IWF-Data-Explorer/",
+    "Weight-Loss-Trends/",
+    "R6-Stats/"
 )
 
 deploy <- function(dir) {
-  library(rsconnect)
-  
-  setAccountInfo(
-    name = "cluffa",
-    token = getenv("TOKEN"),
-    secret = getenv("SECRET")
-  )
-  
-  deployApp(
-    appDir = dir,
-    forceUpdate = TRUE,
-    launch.browser = FALSE
-  )
+    tryCatch(
+        {
+            library(rsconnect)
+            setAccountInfo(
+                name = "cluffa",
+                token = getenv("TOKEN"),
+                secret = getenv("SECRET")
+            )
+            
+            deployApp(
+                appDir = dir,
+                forceUpdate = TRUE,
+                launch.browser = FALSE
+            )
+            
+            out <- paste0(dir, "Deployed Successfully")
+            message(out)
+            return(out)
+        },
+        error = function(e) {
+            message("An Error Occurred")
+            return(e)
+        },
+        warning = function(w) {
+            message("A Warning Occurred")
+            return(w)
+        }
+    )
 }
 
 foreach(dir = dirs) %dopar% deploy(dir)
 
 stopCluster(myCluster)
+
