6f0419a8a5545c87da702a49f5edc42149fbc5fe
cluffa <alexcluff16@gmail.com>
2023-01-21 00:01:32 -0500

    fix

--- deploy.R
+++ deploy.R
@@ -11,34 +11,22 @@ SECRET <- getenv("SECRET")
 DIR <- getenv("DIR")
 
 deploy <- function(dir) {
-    tryCatch(
-        {
-            library(rsconnect)
-            setAccountInfo(
-                name = "cluffa",
-                token = TOKEN,
-                secret = SECRET
-            )
-            
-            deployApp(
-                appDir = dir,
-                forceUpdate = TRUE,
-                launch.browser = FALSE
-            )
-            
-            out <- paste0(dir, "Deployed Successfully")
-            message(out)
-            return(out)
-        },
-        error = function(e) {
-            message("An Error Occurred")
-            return(e)
-        },
-        warning = function(w) {
-            message("A Warning Occurred")
-            return(w)
-        }
+    library(rsconnect)
+    setAccountInfo(
+        name = "cluffa",
+        token = TOKEN,
+        secret = SECRET
     )
+    
+    deployApp(
+        appDir = dir,
+        forceUpdate = TRUE,
+        launch.browser = FALSE
+    )
+    
+    out <- paste0(dir, "Deployed Successfully")
+    message(out)
+    return(out)
 }
 
 deploy(DIR)
