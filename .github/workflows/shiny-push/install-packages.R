050d7334f5f9a06b414cf89ad44c13daf2c3de56
cluffa <cluffa@users.noreply.github.com>
2023-01-26 23:56:45 -0500

    add docker

---
+++ .github/workflows/shiny-push/install-packages.R
@@ -0,0 +1,19 @@
+packages <- c(
+    "rsconnect",
+    "reactable",
+
+    #"tidyverse",
+    "dplyr",
+    "tidyr",
+    "ggplot2",
+    "lubridate",
+    "readr",
+
+    "shinydashboard",
+    "Cairo",
+    "shinyjs",
+    "shinyWidgets"
+)
+
+install.packages(packages, repos = "http://cran.us.r-project.org")
+lapply(packages, library, character.only = TRUE)
